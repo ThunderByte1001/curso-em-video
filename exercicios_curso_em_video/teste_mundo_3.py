bebaac03fd1bcf8f0739f2e5268abdb904481567
Daniel <130584997+ThunderByte1001@users.noreply.github.com>
2023-05-22 19:00:27 +0100

    Add files via upload

---
+++ exercicios_curso_em_video/teste_mundo_3.py
@@ -0,0 +1,30 @@
+'''num = [3,6,4,1]
+for n1,n2 in enumerate(num):
+    print(n1 + n2)'''
+
+'''letras = ('J', 'X', 'M', 'O', 'A', 'K')
+print(letras.index('A'))'''
+
+'''num = [8, 2, 4, 2, 5, 2]
+print(num.count(2))'''
+
+'''pessoas = [['Pedro', 25], ['Maria', 12], ['José', 25]]
+print(pessoas[2][1])'''
+
+'''num = [2, 8, 4, 7]
+num.pop()
+num.insert(1, 3)
+num.append(6)
+print(num)'''
+
+'''num = [6, 2, 1, 4, 3]
+num.sort(reverse=True)
+print(num)'''
+
+'''pontos = [5,2,6,7,3,1]
+print(sorted(pontos))'''
+
+'''paises = ['portugal','espanha','frança']
+print(paises[0],paises[-1])'''
+val = list(range(1, 5))
+print(val)
